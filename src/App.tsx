2bf71db1f3b6c6ed9ddde314dded6768ef060743
José Victor <jose08.victor@gmail.com>
2024-02-20 21:25:26 -0300

    building pages of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Header } from "./components/header";
-import { Home } from "./pages/home";
+import { Crew } from "./pages/crew";
+// import { Destination } from "./pages/destination";
+// import { Home } from "./pages/home";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home />
+      
+      {/* <Home /> */}
+
+      {/* <Destination /> */}
+
+      <Crew />
     </>
   )
 }
