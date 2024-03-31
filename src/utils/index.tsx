3e149b27dbd72f0de683f81c1ef0df741cf4031d
José Victor <jose08.victor@gmail.com>
2024-03-31 13:04:38 -0300

    Update: render component added

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,19 @@
+import { render } from '@testing-library/react';
+import { DataContext } from '../contexts';
+import { ContextType } from '@/types';
+import Data from "../json/data.json";
+
 export const changeClass = (element: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
     document.querySelectorAll('li').forEach((e) => e.classList.remove('active'));
     element.currentTarget.classList.add('active');
+}
+
+export const renderComponent = (Component: JSX.Element, value?: ContextType) => {
+    const Context = { data: Data }
+    
+    return render(
+        <DataContext.Provider value={!value ? Context : value}>
+            {Component}
+        </DataContext.Provider>
+    );
 }
